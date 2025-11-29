170aa843f68d0b3feab815ee663e81b9e3093c51
amreenshahid <amreennshahidd@gmail.com>
2025-11-28 22:06:31 -0500

    first step: loading rubric and exam file into shared memory and also created TA processes

--- src/markingSystem.c
+++ src/markingSystem.c
@@ -7,6 +7,9 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+#include <sys/mman.h>
+
+
 
 
 
@@ -20,6 +23,26 @@ int main(int argc, char *argv[]) {
         printf("Must have at least 2 TAs");
         return 1;
     }
+
+    //------------- Before creating processes, we have to create shared memory -------------//
+
+    //Create shared rubric memory
+    Rubric *shared_mem_rubric = mmap(NULL, sizeof(Rubric), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    if(shared_mem_rubric == MAP_FAILED) {
+        perror("Failed to create shared memory for rubric");
+        exit(1);
+    }
+
+    //Create shared exam memory
+    Exam *shared_mem_exam = mmap(NULL, sizeof(Exam), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    if(shared_mem_exam == MAP_FAILED) {
+        perror("Failed to create shared memory for exam");
+        exit(1);
+    }
+
+    load_rubric("rubric.txt", shared_mem_rubric);   //load rubric file into shared memory
+    load_exams("exams/exam1.txt", shared_mem_exam); //load first exam file into shared memory   
+    //----------------------------------------------------------------------------------------//
 
     //Create processses for each TA using fork
     for(int i = 1; i <= TA; i++) {
@@ -36,9 +59,14 @@ int main(int argc, char *argv[]) {
         }
     }
 
+    //Parent process waits for the TAs 
     for(int i = 0; i < TA; i++) {
-        wait(NULL); //Parent process waits for the TAs 
+        wait(NULL); 
     }
+
+    //deallocate shared memory
+    munmap(shared_mem_rubric, sizeof(Rubric));
+    munmap(shared_mem_exam, sizeof(Exam));
 
     return 0;
 }
